e9720a023bb10e1d4e8712bf11a0707b809c4936
trofa13 <trofa13@inbox.ru>
2017-01-04 14:28:02 +0300

    implemented webpack loading for styles(dev mode)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/js'
+    },
+    output: {
+        path: './dev',
+        filename: "js/[name].js"
+    },
+    devtool: "inline-source-map",
+    module:{
+        loaders:[
+            {
+                test: /\.(scss|sass)$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader!sass-loader")
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("css/[name].css"),
+        new StyleLintPlugin({syntax: 'scss'}),
+        new HtmlWebpackPlugin({
+            "template": "html!src/index.html"
+        }),
+    ]
+}
